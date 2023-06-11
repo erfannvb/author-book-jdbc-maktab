663c108ddf8c48baec58fa0c7a56337f684dc382
Erfan <erfannavab@gmail.com>
2023-06-11 20:56:22 +0330

    - added jdbc connections

---
+++ src/main/java/org/example/connection/JdbcConstant.java
@@ -0,0 +1,11 @@
+package org.example.connection;
+
+public class JdbcConstant {
+
+    public static final String URL = "jdbc:postgresql://localhost:5432:book-author-demo-db";
+
+    public static final String USERNAME = "erfan";
+
+    public static final String PASSWORD = "123456789Erfan";
+
+}
