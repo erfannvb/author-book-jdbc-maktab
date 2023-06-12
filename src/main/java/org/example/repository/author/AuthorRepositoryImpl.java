fe0a86e0ec0c49f1a2aae658cb09a2106f267e96
Erfan <erfannavab@gmail.com>
2023-06-12 19:06:34 +0330

    - implemented loadAll method

---
+++ src/main/java/org/example/repository/author/AuthorRepositoryImpl.java
@@ -0,0 +1,164 @@
+package org.example.repository.author;
+
+import org.example.connection.JdbcConnection;
+import org.example.entity.Author;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.example.repository.author.AuthorQueries.INSERT_INTO_AUTHOR;
+import static org.example.repository.author.AuthorQueries.SELECT_ALL;
+
+public class AuthorRepositoryImpl implements AuthorRepository {
+
+    private Connection connection = null;
+
+    private PreparedStatement preparedStatement = null;
+
+    private ResultSet resultSet = null;
+
+    @Override
+    public void save(Author author) {
+        try {
+
+            connection = JdbcConnection.getConnection();
+            if (connection == null)
+                System.out.println("Error getting the connection.");
+
+            preparedStatement = connection.prepareStatement(INSERT_INTO_AUTHOR);
+
+            preparedStatement.setString(1, author.getFirstName());
+            preparedStatement.setString(2, author.getLastName());
+            preparedStatement.setInt(3, author.getAge());
+
+            preparedStatement.execute();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                JdbcConnection.closeConnection(connection);
+                JdbcConnection.closePreparedStatement(preparedStatement);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public Long saveAndReturnAuthorId(Author author) {
+        Long authorId;
+        try {
+
+            connection = JdbcConnection.getConnection();
+            if (connection == null)
+                System.out.println("Error getting the connection.");
+
+            preparedStatement = connection.prepareStatement(INSERT_INTO_AUTHOR,
+                    PreparedStatement.RETURN_GENERATED_KEYS);
+
+            preparedStatement.setString(1, author.getFirstName());
+            preparedStatement.setString(2, author.getLastName());
+            preparedStatement.setInt(3, author.getAge());
+
+            preparedStatement.execute();
+
+            resultSet = preparedStatement.getGeneratedKeys();
+            resultSet.next();
+            authorId = resultSet.getLong("author_id");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                JdbcConnection.closeConnection(connection);
+                JdbcConnection.closePreparedStatement(preparedStatement);
+                JdbcConnection.closeResultSet(resultSet);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return authorId;
+    }
+
+    @Override
+    public Author load(Long authorId) {
+        Author author = new Author();
+        try {
+
+            connection = JdbcConnection.getConnection();
+            if (connection == null)
+                System.out.println("Error getting the connection.");
+
+            preparedStatement = connection.prepareStatement(INSERT_INTO_AUTHOR);
+            preparedStatement.setLong(1, authorId);
+
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Long id = resultSet.getLong("author_id");
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                int age = resultSet.getInt("age");
+                System.out.println(id + ": " + firstName + " " + lastName + " " + age);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                JdbcConnection.closeConnection(connection);
+                JdbcConnection.closePreparedStatement(preparedStatement);
+                JdbcConnection.closeResultSet(resultSet);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return author;
+    }
+
+    @Override
+    public Author[] loadAll() {
+        Author[] authors;
+        try {
+
+            connection = JdbcConnection.getConnection();
+            if (connection == null)
+                System.out.println("Error getting the connection.");
+
+            preparedStatement = connection.prepareStatement(SELECT_ALL,
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY);
+
+            int size = 0;
+            resultSet.last();
+            size = resultSet.getRow();
+            resultSet.beforeFirst();
+
+            authors = new Author[size];
+
+            int count = 0;
+
+            while (resultSet.next()) {
+                Author author = new Author();
+                author.setFirstName(resultSet.getString("first_name"));
+                author.setLastName(resultSet.getString("last_name"));
+                author.setAge(resultSet.getInt("age"));
+                authors[count++] = author;
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                JdbcConnection.closeConnection(connection);
+                JdbcConnection.closePreparedStatement(preparedStatement);
+                JdbcConnection.closeResultSet(resultSet);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return authors;
+    }
+}
---
+++ src/main/java/org/example/repository/book/BookRepositoryImpl.java
@@ -0,0 +1,152 @@
+package org.example.repository.book;
+
+import org.example.connection.JdbcConnection;
+import org.example.entity.Book;
+
+import java.sql.*;
+
+import static org.example.repository.book.BookQueries.*;
+
+public class BookRepositoryImpl implements BookRepository {
+
+    private Connection connection = null;
+
+    private PreparedStatement preparedStatement = null;
+
+    private ResultSet resultSet = null;
+
+    @Override
+    public void save(Book book) {
+        try {
+            connection = JdbcConnection.getConnection();
+            if (connection == null)
+                System.out.println("Error getting the connection.");
+
+            preparedStatement = connection.prepareStatement(INSERT_INTO_BOOK);
+
+            preparedStatement.setString(1, book.getTitle());
+            preparedStatement.setDate(2, (Date) book.getPublishedYear());
+            preparedStatement.setLong(3, book.getAuthorId());
+
+            preparedStatement.execute();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                JdbcConnection.closeConnection(connection);
+                JdbcConnection.closePreparedStatement(preparedStatement);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public Book load(Long bookId) {
+        Book book = new Book();
+        try {
+
+            connection = JdbcConnection.getConnection();
+            if (connection == null)
+                System.out.println("Error getting the connection.");
+
+            preparedStatement = connection.prepareStatement(SELECT_BOOK_BY_ID);
+            preparedStatement.setLong(1, bookId);
+
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Long id = resultSet.getLong("book_id");
+                String title = resultSet.getString("title");
+                Date publishedYear = resultSet.getDate("published_year");
+                Long authorId = resultSet.getLong("author_id");
+                System.out.println(id + ": " + title + " " + publishedYear + " " + authorId);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                JdbcConnection.closeConnection(connection);
+                JdbcConnection.closePreparedStatement(preparedStatement);
+                JdbcConnection.closeResultSet(resultSet);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return book;
+    }
+
+    @Override
+    public Book[] loadAll() {
+        Book[] books;
+        try {
+
+            connection = JdbcConnection.getConnection();
+            if (connection == null)
+                System.out.println("Error getting the connection.");
+
+            preparedStatement = connection.prepareStatement(SELECT_ALL,
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY);
+
+            resultSet = preparedStatement.executeQuery();
+
+            int size = 0;
+            resultSet.last();
+            size = resultSet.getRow();
+            resultSet.beforeFirst();
+
+            books = new Book[size];
+
+            int count = 0;
+
+            while (resultSet.next()) {
+                Book book = new Book();
+                book.setBookId(resultSet.getLong("book_id"));
+                book.setTitle(resultSet.getString("title"));
+                book.setPublishedYear(resultSet.getDate("published_year"));
+                book.setAuthorId(resultSet.getLong("author_id"));
+                books[count++] = book;
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                JdbcConnection.closeConnection(connection);
+                JdbcConnection.closePreparedStatement(preparedStatement);
+                JdbcConnection.closeResultSet(resultSet);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return books;
+    }
+
+    @Override
+    public void delete(Book book) {
+        try {
+
+            connection = JdbcConnection.getConnection();
+            if (connection == null)
+                System.out.println("Error getting the connection.");
+
+            preparedStatement = connection.prepareStatement(DELETE_BOOK);
+            preparedStatement.setLong(1, book.getBookId());
+
+            preparedStatement.execute();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                JdbcConnection.closeConnection(connection);
+                JdbcConnection.closePreparedStatement(preparedStatement);
+                JdbcConnection.closeResultSet(resultSet);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
